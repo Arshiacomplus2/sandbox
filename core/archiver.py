7e3e1d41b2cd9a5b44e0168bc2758b9ef98158ab
arshiacomplus <arshiacomplus@gmail.com>
2026-05-02 15:09:36 +0330

    New version 18 / fiexed upper than 100mg

--- core/archiver.py
+++ core/archiver.py
@@ -4,7 +4,6 @@ import asyncio
 import re
 
 def sanitize_filename(name):
-
     return re.sub(r'[^\w\.\-]', '_', name)
 
 async def process_archive(file_path: str, comp_mode: str, password: str, updater):
@@ -17,16 +16,17 @@ async def process_archive(file_path: str, comp_mode: str, password: str, updater
     base_name = sanitize_filename(raw_base)
     unique_id = str(uuid.uuid4())[:8]
     new_base = f"{base_name}_{unique_id}"
+    dir_name = os.path.dirname(file_path)
 
 
     if comp_mode == "raw" and file_size_mb <= 95:
-        final_path = os.path.join(os.path.dirname(file_path), f"{new_base}{ext}")
+        final_path = os.path.join(dir_name, f"{new_base}{ext}")
         os.rename(file_path, final_path)
         return [final_path]
 
-    zip_path = os.path.join(os.path.dirname(file_path), f"{new_base}.zip")
-    cmd = ["7z", "a", "-tzip", "-mx=9"]
 
+    zip_path = os.path.join(dir_name, f"{new_base}.zip")
+    cmd = ["7z", "a", "-tzip", "-mx=9"]
 
     if file_size_mb > 95:
         cmd.append("-v95m")
@@ -36,11 +36,24 @@ async def process_archive(file_path: str, comp_mode: str, password: str, updater
 
     cmd.extend([zip_path, file_path])
 
-    process = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    process = await asyncio.create_subprocess_exec(
+        *cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
     await process.wait()
 
-    if os.path.exists(file_path): os.remove(file_path)
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
-    dir_name = os.path.dirname(file_path)
-    generated_files =[os.path.join(dir_name, f) for f in os.listdir(dir_name) if f.startswith(new_base + ".zip")]
+
+    generated_files = [
+        os.path.join(dir_name, f)
+        for f in os.listdir(dir_name)
+        if f.startswith(new_base + ".zip")
+    ]
+
+    if not generated_files:
+        raise Exception("Archiving failed! No output files found.")
+
     return sorted(generated_files)
